562b2c3b1fa928f55c3a978da3b38919515f2d36
Gutemberg Nunes Freitas do Nascimento <g.a.do.nascimento@avanade.com>
2022-06-17 08:41:06 -0300

    CalculadoraMVC

---
+++ CalculadoraMVC/Models/CalculadoraViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CalculadoraMVC.Models
+{
+    public class CalculadoraViewModel
+    {
+        /// <summary>
+        /// Numero 1
+        /// </summary>
+        [Display(Name = "Numero 1")]
+        public int Num1 { get; set; }
+        /// <summary>
+        /// Numero 2
+        /// </summary>
+        [Display(Name = "Numero 2")]
+        public int Num2 { get; set; }
+        /// <summary>
+        /// Operador + - * /
+        /// </summary>
+        public string Operador { get; set; }
+        ///// <summary>
+        ///// Resultado da operacao
+        ///// </summary>
+        public int? Resultado { get; set; }
+    }
+}
+
